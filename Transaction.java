c3b280027d9893daef76287961111b279d65f3c0
donatgosalci <donat.gosalci@student.uni-pr.edu>
2024-05-16 23:54:02 +0200

    Added support for withdrawals, deposits, and transfers in Transaction class

--- Transaction.java
+++ Transaction.java
@@ -1,16 +1,45 @@
 public class Transaction {
+    public enum TransactionType {
+        TRANSFER,
+        WITHDRAWAL,
+        DEPOSIT
+    }
+
+    private TransactionType type;
     private Account sender;
     private Account receiver;
     private double amount;
 
-    public Transaction(Account sender, Account receiver, double amount) {
+    public Transaction(Account sender, Account receiver, double amount, TransactionType type) {
         this.sender = sender;
         this.receiver = receiver;
         this.amount = amount;
+        this.type = type;
     }
 
     public void execute() {
-        sender.withdraw(amount);
-        receiver.deposit(amount);
+        switch (type) {
+            case TRANSFER:
+                if (sender.getBalance() >= amount) {
+                    sender.withdraw(amount);
+                    receiver.deposit(amount);
+                    System.out.println("Transfer Successful");
+                } else {
+                    System.out.println("Insufficient balance for transfer");
+                }
+                break;
+            case WITHDRAWAL:
+                if (sender.getBalance() >= amount) {
+                    sender.withdraw(amount);
+                    System.out.println("Withdrawal Successful");
+                } else {
+                    System.out.println("Insufficient balance for withdrawal");
+                }
+                break;
+            case DEPOSIT:
+                receiver.deposit(amount);
+                System.out.println("Deposit Successful");
+                break;
+        }
     }
 }
